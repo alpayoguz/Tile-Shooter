36693a9ca9078e7ecab40ed4708b3fc99dead6f3
Alpay Oğuzhan Palabıyık <alpayoguz97@gmail.com>
2021-08-03 22:21:34 +0300

    added a random obstacle system

--- Assets/Scripts/Utility.cs
+++ Assets/Scripts/Utility.cs
@@ -7,21 +7,24 @@ public static class Utility
 
 
    
-   public static  T[] ShuffleArray<T>(T[] array, int seed)
+   public static T[] ShuffleArray<T>(T[] array, int seed)
     {
-        System.Random rnd = new System.Random(seed);
+        System.Random prng = new System.Random(seed);
 
-        for(int i = 0; i < array.Length - 1; i++)
+        for (int i = 0; i < array.Length - 1; i++)
         {
-            var randomIndex = rnd.Next(array.Length);
-            var tempItem = array[randomIndex];
+            int randomIndex = prng.Next(i, array.Length);
+            T tempItem = array[randomIndex];
             array[randomIndex] = array[i];
-            array[i] = tempItem; 
+            array[i] = tempItem;
         }
 
 
         return array;
     }
+      
+
+      
 
 
    
